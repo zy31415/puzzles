73b291a74317322f36f25269e071af914989e994
Yang Zhang <zy31415@gmail.com>
2017-03-19 20:59:11 -0700

    .

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,36 @@
+#include <unistd.h>
+
 #include "puzzle/puzzle.h"
 #include "puzzle/printer.h"
 #include "puzzle/dfs.h"
 
-int main() {
-    
-    Puzzle::set_size(3);
-    
-    int arr [] = {1,2,0,3,4,5,6,7,8};
-    
-    Puzzle puzzle(arr);
-    
-    State state(puzzle);
-    
-    Node node(state);
-    
-    DFS dfs;
-    
-    dfs.search(&node);
+int main(int argc, char **argv) {
+    int c;
+    char *fvalue;
 
+    while ((c = getopt (argc, argv, "f:")) != -1)
+        switch (c)
+        {
+            case 'f':
+                fvalue = optarg;
+                break;
+            case '?':
+                if (optopt == 'f')
+                    fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+                else if (isprint (optopt))
+                    fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+                else
+                    fprintf (stderr,
+                             "Unknown option character `\\x%x'.\n",
+                             optopt);
+                return 1;
+            default:
+                abort ();
+        }
+    printf ("fvalue = %s\n", fvalue);
+
+    for (int index = optind; index < argc; index++)
+        printf ("Non-option argument %s\n", argv[index]);
     return 0;
+    
 }
